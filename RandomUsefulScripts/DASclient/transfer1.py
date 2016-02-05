07708bcfd8e46564e5141667238d0cab0f090a43
jrcastle <james.castle@ku.edu>
2016-02-05 10:13:16 -0600

    Add Transfer Parallelization

---
+++ RandomUsefulScripts/DASclient/transfer1.py
@@ -0,0 +1,23 @@
+import os
+
+
+f = open("files1.txt")
+
+fileList = f.readlines()
+Nfiles = len(fileList)
+count = 1
+
+for line in fileList:
+    line = line.replace("\n","")
+    line = line.replace("/lio/lfs/cms","")
+    fName = line.split("/")[9]
+    if os.path.exists(fName):
+        command = "rm " + str(fName)
+        os.system(command)
+    command = "xrdcp root://cmsxrootd.fnal.gov/" + str(line) + " " + str(fName)
+    print "Transferring file " + str(fName) + " ...\t" + str(100.*float(count)/Nfiles) + "% Complete"
+    os.system(command)
+    count = count + 1
+#END FOR
+
+print "DONE!"
